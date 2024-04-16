f225891b46a84321891bed5e0decd89791bd51ca
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-04-16 16:24:59 +0530

    fake database setup

--- src/main/java/com/ajay/Main.java
+++ src/main/java/com/ajay/Main.java
@@ -4,17 +4,48 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
 @SpringBootApplication
 @RestController
 public class Main {
+
+    //db
+    private static List<Customer> customers;
+
+    static {
+
+        customers = new ArrayList<>();
+        Customer alex = new Customer(
+                1,
+                "Alex",
+                "alex@gmail.com",
+                21
+        );
+
+        Customer jamila = new Customer(
+                2,
+                "Jamila",
+                "jamila@gmail.com",
+                22
+        );
+
+        customers.add(alex);
+        customers.add(jamila);
+
+    }
+
     public static void main(String[] args) {
+
+        System.out.println(customers);
+
         SpringApplication.run(Main.class, args);
     }
 
-    class Customer{
+    static class Customer{
         private Integer id;
         private String name;
         private String email;
